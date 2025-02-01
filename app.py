147f4693cf45707a9b7573f50194be69cc5c130d
diogobitten <diogobittenc@gmail.com>
2025-02-01 11:04:40 -0300

    modify host to 0.0.0.0

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def chat():
         return jsonify({'response': f'😔 Ocorreu um erro: {e}'}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
